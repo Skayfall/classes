4c9ef111e790da920d4a070423933898c94d5430
Skayfall <58567973+Skayfall@users.noreply.github.com>
2020-11-05 14:30:32 +0300

    Update Button.py

--- Button.py
+++ Button.py
@@ -1,9 +1,12 @@
 class Button:
     def __init__(self):
         self.a == 0
+        
     def click(self):
         self.a += 1
+        
     def click_count(self):
         return self.a
+    
     def reset(self):
         self.a == 0

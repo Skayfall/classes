6020913cb98ffb69dfba366ec56a025cfd5fcda5
Skayfall <58567973+Skayfall@users.noreply.github.com>
2020-11-05 15:01:12 +0300

    Update Balance.py

--- Balance.py
+++ Balance.py
@@ -1,18 +1,18 @@
 class Balance:
     def __init__(self):
-        a == 0
-        b == 0
+        a = 0
+        b = 0
 
     def add_right(self, a):
         self.a = a
 
     def add_left(self, b):
         self.b = b
-        
+
     def result(self):
         if self.a == self.b:
             return '='
         elif self.a > self.b:
             return 'R'
         else:
-            return "L"
+            return 'L'
